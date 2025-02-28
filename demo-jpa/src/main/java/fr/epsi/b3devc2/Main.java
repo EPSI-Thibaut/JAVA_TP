612dbdb87f4ce8a3bb50f02017853bc1f4647b91
Thibaut Mosteau <thibaut.mosteau@lilo.org>
2025-02-28 10:47:12 +0100

    demo-jpa connection OK BDD

--- demo-jpa/src/main/java/fr/epsi/b3devc2/Main.java
+++ demo-jpa/src/main/java/fr/epsi/b3devc2/Main.java
@@ -1,10 +1,17 @@
 package fr.epsi.b3devc2;
 
 import javax.swing.text.html.parser.Entity;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.EntityManager;
+
+
 
 public class Main {
     public static void main(String[] args) {
-        try(EntityManagerFactory emf = Persistance.)
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("demo-jpa");
+             EntityManager em = emf.createEntityManager()) {
+            System.out.println("Connection réussie");
+        }
     }
-
 }
