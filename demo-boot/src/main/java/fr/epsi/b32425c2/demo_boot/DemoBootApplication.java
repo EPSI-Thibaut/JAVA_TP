0ce42808c7612fab36c567a1c6216a0219c33aa7
Thibaut Mosteau <thibaut.mosteau@lilo.org>
2025-03-18 14:13:05 +0100

    Spring – TP 02 – Montrer l’injection de dépendance

--- demo-boot/src/main/java/fr/epsi/b32425c2/demo_boot/DemoBootApplication.java
+++ demo-boot/src/main/java/fr/epsi/b32425c2/demo_boot/DemoBootApplication.java
@@ -1,5 +1,7 @@
 package fr.epsi.b32425c2.demo_boot;
 
+import fr.epsi.b32425c2.demo_boot.service.BavardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoBootApplication {
 
+	@Autowired
+	private BavardService bavardService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoBootApplication.class, args);
 	}
@@ -16,5 +21,10 @@ public class DemoBootApplication {
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello world";
+	}
+	// variante de l'étape 3 du tp (bonus)
+	@GetMapping("/blabla")
+	public String blabla() {
+		return bavardService.parler();
 	}
 }
---
+++ demo-boot/src/main/java/fr/epsi/b32425c2/demo_boot/service/BavardService.java
@@ -0,0 +1,34 @@
+package fr.epsi.b32425c2.demo_boot.service;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+// 1 - Création d'une classe BavardService
+@Component
+public class BavardService {
+    private String nom = "Valeur par défaut";
+
+    // Getter
+    public String getNom() {
+        return nom;
+    }
+
+    // Setter
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    // Méthode parler qui affiche le nom et le nom de la classe
+    public String parler() {
+        System.out.println("Nom : " + nom + ", Classe : " + this.getClass().getSimpleName());
+        return null;
+    }
+
+    // 2 - Méthode annotée avec @PostConstruct
+    @PostConstruct
+    private void postConstruct() {
+        System.out.println("BavardService postConstruct");
+    }
+
+
+}
